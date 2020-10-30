00cd22bb266df86980702a2385da2dfe481df858
Lakshya Kumar <kumar.lakshya24@gmail.com>
2020-10-29 17:07:26 -0700

    Updates

---
+++ precache-manifest.48245125f5f943e663ee4e8aad896030.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e00f3d60bd39098a953fd1db3b83248",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a05f37b247d7e8846d6",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0a22d970862575812b61",
+    "url": "/static/css/main.e9084563.chunk.css"
+  },
+  {
+    "revision": "8a05f37b247d7e8846d6",
+    "url": "/static/js/2.74bf0c0e.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.74bf0c0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a22d970862575812b61",
+    "url": "/static/js/main.f309e295.chunk.js"
+  },
+  {
+    "revision": "b9eca87539c29ef36aa0",
+    "url": "/static/js/runtime-main.379e8025.js"
+  },
+  {
+    "revision": "adbc073ac03e1b330eba67031923908f",
+    "url": "/static/media/resume.adbc073a.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.40aa2122b1bb49f8c86ec65a971e36c8.js"
+  "/precache-manifest.48245125f5f943e663ee4e8aad896030.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
